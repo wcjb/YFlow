c9c48f3a417f09f50d0ddc12cd9b1fd33b820029
威池军博 <1045612428@qq.com>
2021-02-18 16:33:41 +0800

    新增Remez算法相关文档、修订README文档

--- source/tools.h
+++ source/tools.h
@@ -55,8 +55,9 @@ float InvSqrt (float x);
 int IsBigEndian();
 
 long long binpow(long long a, long long b);
+
 /**
- * @brief 参考1993年由Sun Microsystems写入C标准库的方案的log函数算法，函数为 double ieee754log(double x)（IEEE754: IEEE二进制浮点数算术标准）
+ * @brief 基于泰勒级数和Remez算法的ln函数计算实现
  * @param  x  要取自然对数的值
  * @note                  
  *   1. 参数约化（Argument Reduction）: 找到k和f使得
@@ -64,62 +65,55 @@ long long binpow(long long a, long long b);
  *	   其中  sqrt(2)/2 < 1+f < sqrt(2) .
  *	   则有 log(x) = k*log2+log(1+f) 
  *   2.  计算log(1+f)的近似值.
- *	  做变换s = f/(2+f) ; 则有log(1+f) = log(1+s) - log(1-s)
+ *	  作变换s = f/(2+f) ; 则有log(1+f) = log(1+s) - log(1-s)
  *		 = 2s + 2/3 s**3 + 2/5 s**5 + ...
  *	     = 2s + s*R
- *      We use a special Reme algorithm on [0,0.1716] to generate 
- * 	a polynomial of degree 14 to approximate R The maximum error 
- *	of this polynomial approximation is bounded by 2**-58.45. In
- *	other words,
- *		        2      4      6      8      10      12      14
+ *      接下来在区间[0,0.1716]使用Remez算法来计算多项式R的最佳一致逼近
+ * 多项式，理论上，度数为14的最佳一致逼近多项式的精度为2**-58.45，完全能
+ * 满足64位浮点数的运算要求。
+ *		得到最佳一致多项式如下：
  *	    R(z) ~ Lg1*s +Lg2*s +Lg3*s +Lg4*s +Lg5*s  +Lg6*s  +Lg7*s
- *  	(the values of Lg1 to Lg7 are listed in the program)
- *	and
- *	    |      2          14          |     -58.45
- *	    | Lg1*s +...+Lg7*s    -  R(z) | <= 2 
- *	    |                             |
- *	Note that 2s = f - s*f = f - hfsq + s*hfsq, where hfsq = f*f/2.
- *	In order to guarantee error in log below 1ulp, we compute log
- *	by
- *		log(1+f) = f - s*(f - R)	(if f is not too large)
- *		log(1+f) = f - (hfsq - s*(hfsq+R)).	(better accuracy)
+ *  	( Lg1 到 Lg7 的值已在程序种给出)
+ *	此时，
+ *	   
+ *	    | Lg1*s +...+Lg7*s    -  R(z) | <= 2**-58.45
+ *
+ *	然后根据2s = f - s*f = f - hfsq + s*hfsq, 其中 hfsq = f*f/2.
+ *  得到
+ *		log(1+f) = f - s*(f - R)	
+ * 如果对精度有更高要求，则继续迭代2s=f-sf,得到：
+ *		log(1+f) = f - (hfsq - s*(hfsq+R)).	
  *	
- *	3. Finally,  log(x) = k*ln2 + log(1+f).  
+ *	3. 最终,  log(x) = k*ln2 + log(1+f).  
  *			    = k*ln2_hi+(f-(hfsq-(s*(hfsq+R)+k*ln2_lo)))
- *	   Here ln2 is split into two floating point number: 
- *			ln2_hi + ln2_lo,
- *	   where n*ln2_hi is always exact for |n| < 2000.
- *
- * Special cases:
- *	log(x) is NaN with signal if x < 0 (including -INF) ; 
- *	log(+INF) is +INF; log(0) is -INF with signal;
- *	log(NaN) is that NaN with no signal.
- *
- * Accuracy:
- *	according to an error analysis, the error is always less than
- *	1 ulp (unit in the last place).
- *
- * Constants:
- * The hexadecimal values are the intended ones for the following 
- * constants. The decimal values may be used, provided that the 
- * compiler will convert from decimal to binary accurately enough 
- * to produce the hexadecimal values shown.
+ *     需要注意的是，此处将ln2分成了两部分进行计算：
+ *                   ln2 = ln2_hi + ln2_lo
+ *	   当|n| < 2000时n*ln2_hi是精确的。
  *
  * @return float @c
  * @see 参考知乎回答的第一条答案：https://www.zhihu.com/question/333371020/answer/1686069171
+ * 关于Remez算法可参看项目路径/docs下的Function approximation and the remez algorithm和数值计
+ * 算方法等文档。 算法实现可参考C语言math库源码http://www.netlib.org/fdlibm/。
  */
- //http://www.netlib.org/fdlibm/
-// #if defined 检查常量是否被定义
-double __ieee754_log(double x);
 
-//由于计算机无法存储浮点数的精确值（浮点数的存储方法可以参考 IEEE 754，这里不再赘述），而指数函数和对数函数的参数和返回值均为浮点数，因此运算过程中会存在误差。
+double __ieee754_log(double x);  //ln的remes
 
+double __simpson_log(double x)
 /**
  * @brief 实现任意底数n的对数函数
+ * 由于计算机无法存储浮点数的精确值（浮点数的存储方法可以参考 IEEE 754，这里不再赘述），
+ * 而指数函数和对数函数的参数和返回值均为浮点数，因此运算过程中会存在误差。
  * @note log_{n}(x) = ln(x)/ln(n)
  * @param  x                要求对数的对象
  * @param  n                对数的底数
  * @return double @c 
  */
 double log(double x,int n);
-#endif
+
+/**
+ * @brief 平方根函数实现
+ * @param  x                desc
+ * @return float @c 
+ */
+float sqrt(float x);
+#endif 
